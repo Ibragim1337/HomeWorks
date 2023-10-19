77a0848e6d44498300a9c1ce902e50f52e6eab4c
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2023-10-19 23:41:20 +0400

    rest of tasks

--- methods.js
+++ methods.js
@@ -18,3 +18,67 @@ if (arr.length === 0) {
 } */
 
 //----------------------------------------2 задание -------------------------------------------
+
+/* console.clear();
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 99];
+let maxNumber = -Infinity;
+
+if (arr.length === 0) {
+  maxNumber = -Infinity;
+} else {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxNumber) {
+      maxNumber = arr[i];
+    }
+  }
+}
+
+console.log(maxNumber); */
+
+// ---------------------------------- 3 задание ----------------------------------------------
+
+/* const symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const stringLength = 10;
+let randomString = '';
+
+for (const symbol of symbols) {
+  const randomIndex = Math.round(Math.random() * symbols.length);
+  randomString += symbols[randomIndex];
+  if (randomString.length === stringLength) {
+    break;
+  }
+}
+
+console.log(randomString); */
+
+//---------------------------------4 задание -------------------------------------------------
+
+const year = parseInt(prompt("Введите год:"));
+const month = parseInt(prompt("Введите месяц (1-12):"));
+
+let daysInMonth;
+
+if (month < 1 || month > 12) {
+  console.log("Некорректный месяц. Введите значение от 1 до 12.");
+} else {
+  if (month === 4 || month === 6 || month === 9 || month === 11) {
+    daysInMonth = 30;
+  } else if (month === 2) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      daysInMonth = 29;
+    } else {
+      daysInMonth = 28;
+    }
+  } else {
+    daysInMonth = 31;
+  }
+
+  console.log(`В месяце ${month} года ${year} ${daysInMonth} дней.`);
+}
+
+
+
+
+
+
