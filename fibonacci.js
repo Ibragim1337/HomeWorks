1b62340f9cb71a7b2fe5f78ba736a5396c20bb83
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2023-10-25 19:03:47 +0400

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+function fibonacci(number) {
+  if (number <= 0) {
+    return 0;
+  } else if (number === 1) {
+    return 1;
+  } else {
+    let previousNumber = 0;
+    let currentNumber = 1;
+    for (let i = 2; i <= number; i++) {
+      let nextNumber = previousNumber + currentNumber;
+      previousNumber = currentNumber;
+      currentNumber = nextNumber;
+    }
+    return currentNumber;
+  }
+}
